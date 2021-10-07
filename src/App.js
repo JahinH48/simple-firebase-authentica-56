2487f857c10e5531f6c3d37fa0da154ff5ad5170
unknown <jahinhasan48@gmail.com>
2021-10-07 12:19:57 +0600

    jisan

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {getAuth, signInWithPopup ,GoogleAuthProvider ,GithubAuthProvider } from "firebase/auth";
+
+
+import initallzeAuthentication from './Firebase/firebase';
+import { useState } from 'react';
+
+// GitHub 
+const githubProvider = new GithubAuthProvider();
+
+initallzeAuthentication()
+const googleProvider = new GoogleAuthProvider();
 function App() {
+  const [ user , setUser ] = useState({})
+  
+ 
+  const auth = getAuth();
+  const handleGoogleSingIn =()=>{
+    
+    signInWithPopup(auth , googleProvider)
+    .then(result=>{
+        const {displayName , email ,photoURL} = result.user;
+    
+        const logginUser ={
+          name :displayName,
+          email:email,
+          photo : photoURL
+        };
+        setUser(logginUser);
+       
+    })
+    .catch(error =>{
+      console.log(error.massage)
+    })
+  }
+
+  const GithubSignIn = () => {
+    signInWithPopup(auth , githubProvider)
+      .then(result => {
+        const { displayName, photoURL, email } = result.user;
+       
+        console.log(result.user);
+        const logginUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+          
+        }
+        setUser(logginUser);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={handleGoogleSingIn}>Google Sing In </button>
+      <button onClick={GithubSignIn}>GitHub Sing In</button>
+    <br />
+  
+    {
+      user.photo && <div>
+        <h2>WellCome {user.name}</h2>
+        <p>Email: {user.email}</p>
+        <img src={user.photo} alt="" />
+      </div>
+    }
     </div>
   );
 }
---
+++ src/Firebase/Firebase-confic.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9d5_8EcWie3G5rVU689rVNvRz2xQ3__8",
+  authDomain: "simple-firebase-authentica-56.firebaseapp.com",
+  projectId: "simple-firebase-authentica-56",
+  storageBucket: "simple-firebase-authentica-56.appspot.com",
+  messagingSenderId: "1030301892251",
+  appId: "1:1030301892251:web:04dd5db290235e6fb2103e"
+};
+
+export default firebaseConfig;
